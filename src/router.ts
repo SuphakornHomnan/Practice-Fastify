75b96b83d81d3b6c90fe8031e3abb6f3549aeddf
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-03-01 23:33:17 +0700

    [Added] users router

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from 'fastify'
 import indexRoute from './controllers/index'
 import demoRoute from './controllers/demo'
 import testRouter from './controllers/test'
+import userRouter from './controllers/users'
 
 export default async function router(fastify: FastifyInstance) {
     // router prefix
     fastify.register(indexRoute, {prefix: '/'})
     fastify.register(demoRoute, {prefix: '/demo'})
     fastify.register(testRouter, { prefix: '/test'})
+    fastify.register(userRouter, { prefix: '/users'})
 }
